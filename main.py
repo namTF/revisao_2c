46ce4031859033689bc8f5fbd1effc8674072ab5
namtf <reynamtavares948@gmail.com>
2026-07-31 15:58:10 -0300

    versao 1

---
+++ main.py
@@ -0,0 +1,6 @@
+nome=input('Digite o seu nome:')
+nota1=float(input('Digite a nota 1:'))
+def calcular_media(n1,n2):
+    return (n1+n2)/2
+media=calcular_media(nota1,nota2)
+print('Sua media é'{media})
